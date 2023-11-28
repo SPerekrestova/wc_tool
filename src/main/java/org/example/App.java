6aaecb75fc3ad6d58ae864444abd256496eba6d5
sperekrestova <sperekrestova@gopoints.ru>
2023-11-28 20:47:05 +0300

    Added more functionality

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,17 +5,14 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.io.filefilter.NameFileFilter;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.WordUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Locale;
 
 public class App {
     public static void main(String[] args) throws IOException {
